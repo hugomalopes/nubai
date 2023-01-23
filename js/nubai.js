da4e4940d04f13cd4f4afb574374ccfa125ed454
hugomalopes <huguinhoman@gmail.com>
2023-01-23 02:43:56 +0000

    Stats added. Haversine distance, visited countries list, timeline

--- js/nubai.js
+++ js/nubai.js
@@ -12,7 +12,10 @@ const localization_json = {
         "lastupdate": "Última actualização",
         "photo": "Foto",
         "chat": "Fala connosco",
-        "about": "Sobre o local"
+        "about": "Sobre o local",
+        "countries": "Países visitados:",
+        "distance": "Distância percorrida (estimativa simples):",
+        "timeline": "Linha cronológica:"
     },
     "en": {
         "header1": "Nu bai to South America",
@@ -24,13 +27,36 @@ const localization_json = {
         "lastupdate": "Last update",
         "photo": "Photo",
         "chat": "Chat with us",
-        "about": "About the place"
+        "about": "About the place",
+        "countries": "Visited countries:",
+        "distance": "Distance travelled (simple estimate):",
+        "timeline": "Timeline:"
     }
 };
 
 // get pastebin raw data through allorigins to bypass CORS
 // ?nocache=${Date.now()} is used to bypasse allorigins cache system
 let config_json_url = `https://api.allorigins.win/get?url=${encodeURIComponent('https://pastebin.com/raw/WWSYFdfQ')}?nocache=${Date.now()}`;
+
+// haversine formula to determinate the great-circle distance between coordinates
+// kudos to https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
 
 function img_fullscreen_modal(src) {
     var img = document.getElementById("img-fullscreen-modal");
@@ -149,9 +175,33 @@ fetch(config_json_url)
     let map_div = document.getElementById('map-div');
     map_div.appendChild(last_update_p);
 
+    // statistics
+    var visited_countries_list = [];
+    var last_coordinates = {};  // to be used in the following forEach loop
+    var travelled_km_estimate = 0;
+
     // parsing photos list
     var latlngs = [];
     config_json.photo_list.forEach( (p, index, array) => {
+        // resolving different p.address keys for different geographical organizations
+        var address_local_id_list = ["hamlet", "town", "city"];
+        var address_local_found_list = [];
+        address_local_id_list.forEach( al => {
+            if (al in p.address) {
+                address_local_found_list.push(p.address[al]);
+            }
+        });
+        var address_local = address_local_found_list.join(", ");
+
+        var address_state_county_id_list = ["county", "state"];
+        var address_state_county_found_list = [];
+        address_state_county_id_list.forEach( al => {
+            if (al in p.address) {
+                address_state_county_found_list.push(p.address[al]);
+            }
+        });
+        var address_state_county = address_state_county_found_list.join(", ");
+
         // adding coordinate markers
         var marker = L.marker([p.gps_latitude, p.gps_longitude]).addTo(map);
         var popup_html = `
@@ -163,24 +213,6 @@ fetch(config_json_url)
             `;
         marker.bindPopup(popup_html);
         marker.on('click', function() {
-            var address_local_id_list = ["hamlet", "town", "city"];
-            var address_local_found_list = [];
-            address_local_id_list.forEach( al => {
-                if (al in p.address) {
-                    address_local_found_list.push(p.address[al]);
-                }
-            });
-            var address_local = address_local_found_list.join(", ");
-
-            var address_state_county_id_list = ["county", "state"];
-            var address_state_county_found_list = [];
-            address_state_county_id_list.forEach( al => {
-                if (al in p.address) {
-                    address_state_county_found_list.push(p.address[al]);
-                }
-            });
-            var address_state_county = address_state_county_found_list.join(", ");
-
             // fill img div with photo
             var image_div = document.getElementById('selected-photo');
             image_div.innerHTML = `
@@ -189,17 +221,46 @@ fetch(config_json_url)
             </a>
             <p>${p.text}</p>
             <p>${p.gps_date_stamp}, ${address_local}, ${address_state_county}, ${p.address.country}</p>`;
+        });
 
-            selected_address = p.address;  // useful to reload wikipedia info with new lang
-            wikipedia_summary(p.address);
-        })
+        selected_address = p.address;  // useful to reload wikipedia info with new lang
+        wikipedia_summary(p.address);
+
+        // stats
+        if (!visited_countries_list.includes(p.address.country)) {
+            visited_countries_list.push(p.address.country);
+        }
+        if (Object.keys(last_coordinates).length > 0) {
+            current_route_leg_distance = getDistanceFromLatLonInKm(last_coordinates.lat, last_coordinates.lon, p.gps_latitude, p.gps_longitude);
+            console.log(last_coordinates);
+            console.log(current_route_leg_distance);
+            travelled_km_estimate += current_route_leg_distance;
+        }
+        last_coordinates = {
+            lat: p.gps_latitude,
+            lon: p.gps_longitude
+        };
+        var stats_timeline_table = document.getElementById('stats-timeline-table');
+        stats_timeline_table.innerHTML += `
+            <tr>
+              <td class="border-dark border-end text-end">${p.gps_date_stamp}</td>
+              <td class="text-start col-md-7">${address_local}, ${address_state_county}, ${p.address.country}</td>
+            </tr>
+        `;
+
         latlngs.push([p.gps_latitude, p.gps_longitude]);
 
         if (index === array.length - 1) {
             marker.fire('click');
         }
-
     });
+
+    // fill stats container
+    var stats_visited_countries = document.getElementById('stats-visited-countries');
+    stats_visited_countries.innerHTML = visited_countries_list.join(", ");
+    var stats_travelled_distance = document.getElementById('stats-travelled-distance');
+    stats_travelled_distance.innerHTML = `${Math.round(travelled_km_estimate)} km`;
+
     var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
     // zoom the map to the polyline
     map.fitBounds(polyline.getBounds());
