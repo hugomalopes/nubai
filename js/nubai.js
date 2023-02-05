d31236fa461d7c9d86ced029075822f5f13c48e9
hugomalopes <huguinhoman@gmail.com>
2023-02-05 01:53:55 +0000

    zoom to last position II

--- js/nubai.js
+++ js/nubai.js
@@ -275,9 +275,11 @@ fetch(config_json_url)
 
     var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
     // zoom the map to the polyline
-    map.fitBounds(polyline.getBounds());
-    // then zoom to the last position
-    map.flyTo(latlngs[latlngs.length - 1]);
+    map.fitBounds(polyline.getBounds()).then( m => {
+      // zoom to the last position
+      map.flyTo(latlngs[latlngs.length - 1]);
+      return map;
+    });
 })
 .catch(err => {
     throw err;
